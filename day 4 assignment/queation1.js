a69e2126985353087fdf4b3c4932bccce8c9cb1f
rahul kumar kasaudhon <68266050+rahulg311@users.noreply.github.com>
2020-07-20 14:37:11 +0530

    Add files via upload

---
+++ day 4 assignment/queation1.js
@@ -0,0 +1,13 @@
+for(var i=0;i<=100;i++){
+    if(i%(3*5) == 0)
+    {
+        console.log('fizzbuzz');
+    }
+    else if(i%3 == 0){
+        console.log('fizz');
+    }
+    else if(i%s == 0)
+    {
+        console.log('buzz');
+    }
+}
